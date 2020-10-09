354d0d218dae57712b6804274320810a1c161f43
ali <ali.hong@daimler.com>
2020-10-09 15:27:32 +0800

    add new

---
+++ src/main/java/controller/SampleController.java
@@ -0,0 +1,87 @@
+package controller;
+
+import net.sf.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import service.MailService;
+import util.EmailUtil;
+import util.SendMessageUtil;
+
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.security.GeneralSecurityException;
+import java.util.Random;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    private MailService mailService;
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+
+    @RequestMapping("/sendMail")
+    @ResponseBody
+    @CrossOrigin
+    ResponseEntity<String> sendMail(HttpServletRequest request, HttpServletResponse response) throws GeneralSecurityException, MessagingException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Cache-Control", "no-cache");
+        String info = "";
+        String name = request.getParameter("name");
+        String phone = request.getParameter("phone");
+        String companyName = request.getParameter("companyName");
+        String email = request.getParameter("email");
+        String content = request.getParameter("content");
+        if (StringUtils.isNotBlank(name) && StringUtils.isNotBlank(phone) && StringUtils.isNotBlank(companyName) && StringUtils.isNotBlank(email)) {
+            info = "姓名：" + name + " 手机号：" + phone + " 公司名称：" + companyName + " 邮箱：" + email + " 内容：" + content;
+            System.out.println("=====" + info);
+            EmailUtil.sendEmail("86984642@qq.com", "ceshi", info);
+            //mailService.sendSimpleMail("tq070@qq.com","test simple mail"," hello this is simple mail");
+            return ResponseEntity.ok("200");
+        } else {
+            return ResponseEntity.ok("400");
+        }
+    }
+
+    @RequestMapping("/sendSMS")
+    @ResponseBody
+    ResponseEntity<String> sendSMS() throws Throwable {
+        //手机号
+        Random random = new Random();
+        int r = random.nextInt(9999);
+        String mobile = "13810525644";
+        String content = "【中交兴路】验证码为" + r + "，请在5分钟内输入，谢谢使用！";
+        String account = "ZJWBGYS";
+        String password = "4HEXC3tM";
+        String sig = "hHzepSbx";
+        /**
+         * 账号信息
+         *   平台编码:ZJWBGYS
+         *   平台密码:4HEXC3tM
+         *   平台签名:hHzepSbx
+         */
+
+        String result = SendMessageUtil.sendMsg(account, password, mobile, content, sig);
+        JSONObject object = JSONObject.fromObject(result);
+        int status = Integer.parseInt(object.get("status").toString());
+        if (status == 0) {
+            return ResponseEntity.ok(r + "");
+        } else {
+            return ResponseEntity.ok("400");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
---
+++ src/main/java/util/SendMessageUtil.java
@@ -0,0 +1,39 @@
+package util;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SendMessageUtil {
+    //http请求发送相同短信
+    public static String sendMsg(String account, String passwd, String mobile, String content, String sig) throws Throwable {
+
+        String url = "http://sms.sinoiov.com/NotificationService/rest/v1/message/sender";
+//		String url="http://localhost:8080/NotificationService/rest/v1/message/sender";
+        Map<String, String> mapHeader = new HashMap<String, String>();
+        //账号
+        mapHeader.put("account", account);
+        //密码
+        String password = MD5Utils.getDefaultMd5String(passwd);
+        System.out.println(password);
+        mapHeader.put("password", password);
+        //请求ID
+        mapHeader.put("requestId", "SMS" + System.currentTimeMillis());
+        Map<String, String> jsonBody = new HashMap<String, String>();
+        //手机号
+        jsonBody.put("mobile", mobile);
+        //短信内容
+        jsonBody.put("content", content);
+        //签名认证
+        String mac = mobile + "_" + passwd + "" + account + "" + sig;
+        jsonBody.put("mac", MD5Utils.getDefaultMd5String(mac));
+        //请不要使用map.toString()方法.
+        String jsonString = net.sf.json.JSONObject.fromObject(jsonBody).toString();
+        JSONObject.toJSONString(mapHeader);
+        System.out.println(jsonString);
+        String result = HttpSendUtils.send(url, mapHeader, jsonString);
+        System.out.println("testSendMsg-->" + result);
+        return result;
+    }
+}
